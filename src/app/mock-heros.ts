869d03c457bcd714b0a5d4eaa11b9cf87bdcb6c4
Manideep572 <69362139+Manideep572@users.noreply.github.com>
2021-03-22 18:07:54 +0530

    3rd topic

--- src/app/mock-heros.ts
+++ src/app/mock-heros.ts
@@ -1,7 +1,14 @@
 import {Hero} from "./hero";
 
-export const Heros:Hero[]=[
+export const HEROES:Hero[]=[
     {id:1,name:"Windstrom"},
+    {id:2,name:"Earth"},
+    {id:3,name:"Meen"},
+    {id:4,name:"Moon"},
+    {id:5,name:"Laptop"},
+    {id:6,name:"Avenger"},
+    {id:7,name:"Java"},
+    {id:8,name:"noor"}
     
 
-]
+];
